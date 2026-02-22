a080e142707542e82a2a8d1cdf0a8de9a9a88a63
Sitthikorn11 <zervew111@gmail.com>
2026-02-22 15:30:53 +0700

    update4

--- src/main/java/com/example/demo/product/dto/SellProductsRequest.java
+++ src/main/java/com/example/demo/product/dto/SellProductsRequest.java
@@ -6,7 +6,7 @@ public class SellProductsRequest {
 
     private BigDecimal moneyMyself;
     private Long amount;
-    private Long ProductsID;
+    private Long productsID;
 
     public BigDecimal getMoneyMyself() {
         return moneyMyself;
@@ -16,19 +16,19 @@ public class SellProductsRequest {
         this.moneyMyself = moneyMyself;
     }
 
-    public Long getProductsID() {
-        return ProductsID;
-    }
-
-    public void setProductsID(Long productsID) {
-        ProductsID = productsID;
-    }
-
     public Long getAmount() {
         return amount;
     }
 
     public void setAmount(Long amount) {
         this.amount = amount;
+    }
+
+    public Long getProductsID() {
+        return productsID;
+    }
+
+    public void setProductsID(Long productsID) {
+        this.productsID = productsID;
     }
 }
